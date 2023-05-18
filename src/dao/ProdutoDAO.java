f1547fc0221d5bbee64309fab8cc3ea5e2c9ccef
Guilherme da Cunha Silveira <182120069@senacinformatica.br>
2023-05-17 21:37:01 -0300

    dao

---
+++ src/dao/ProdutoDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author 182120069
+ */
+public class ProdutoDAO {
+    public void cadastrarProduto (pVO){
+        
+    }
+}
